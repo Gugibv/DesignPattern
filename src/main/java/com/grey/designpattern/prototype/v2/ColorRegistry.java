d3ac36e5bf5fce670bea8030d01edb8699aae6e0
grey <greyjing.zhou@ncsi.com.cn>
2024-01-22 15:39:09 +0800

    更新原型模式代码

--- src/main/java/com/grey/designpattern/prototype/v2/ColorRegistry.java
+++ src/main/java/com/grey/designpattern/prototype/v2/ColorRegistry.java
@@ -3,7 +3,7 @@ package com.grey.designpattern.prototype.v2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ColorStore {
+public class ColorRegistry {
 
     private static Map<String, Color> colorMap = new HashMap<String, Color>();
 
@@ -12,9 +12,15 @@ public class ColorStore {
         colorMap.put("blue", new blueColor());
         colorMap.put("black", new blackColor());
     }
-
+    public static void registerColor(String name, Color color) {
+        colorMap.put(name, color);
+    }
     public static Color getColor(String colorName)
     {
-        return (Color) colorMap.get(colorName).clone();
+        Color color = colorMap.get(colorName);
+        if (color != null) {
+            return  (Color) colorMap.get(colorName).clone();
+        }
+        return null;
     }
 }
--- src/main/java/com/grey/designpattern/prototype/v2/Prototype.java
+++ src/main/java/com/grey/designpattern/prototype/v2/Prototype.java
@@ -3,9 +3,29 @@ package com.grey.designpattern.prototype.v2;
 public class Prototype {
     public static void main (String[] args)
     {
-        ColorStore.getColor("blue").addColor();
-        ColorStore.getColor("black").addColor();
-        ColorStore.getColor("black").addColor();
-        ColorStore.getColor("blue").addColor();
+        ColorRegistry.getColor("blue").addColor();
+        ColorRegistry.getColor("black").addColor();
+
+
+
+        // 注册新的实现类型
+        ColorRegistry.registerColor("yellow", new Color() {
+            @Override
+            void addColor() {
+
+            }
+
+            @Override
+            public Color clone() {
+                return new Color() {
+                    @Override
+                    void addColor() {
+                        System.out.println("Yellow color added");
+                    }
+                };
+            }
+        });
+
+        ColorRegistry.getColor("yellow").addColor();
     }
 }
